eb5901ee678eda9e3885cd6a3628dcc77aa2c939
karen avner <karenavr@gmail.com>
2020-08-11 10:59:49 +0300

    add functionality to portfolio

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -26,3 +26,32 @@ function addRandomFact() {
   const factContainer = document.getElementById('fact-container');
   factContainer.innerText = fact;
 }
+
+/**
+ * Creates a semi-random story and adds it to the page.
+ */
+function randomizeStory(){
+    var inputFromUser = document.getElementById('inputName').value;
+    var inputName = (inputFromUser=='') ? "Karen" : inputFromUser;
+    var objectsList = ["calculator", "adventure book", "arrow", "wand", "ear plugs"]
+    var characterList = ["snail", "witch", "king", "rabbit", "lion", "clown"]
+    var text = "";
+
+    text = text + "This is a story about a " + getRandomItemFromArray(characterList) + 
+        " called " + inputName + ". " + inputName + " had to find the magical " + 
+        getRandomItemFromArray(objectsList) + " in order to cure their beloved sick " + 
+        getRandomItemFromArray(characterList) + ". " + inputName + 
+        " sacrifised everything for it, including losing their favourite " + 
+        getRandomItemFromArray(objectsList) + 
+        ". But it was all worth it, and they lived happily ever after."
+
+    document.getElementById("storyBox").innerHTML=text;
+document.getElementById("storyBox").style.visibility='visible';
+}
+
+/**
+* returns a semi-ramdom item from the given array.
+*/ 
+function getRandomItemFromArray(array){
+    return array[Math.floor(Math.random()*(array.length))];
+}
