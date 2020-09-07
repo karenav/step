c09324d0deebb4eaafb6dd2980a3a139f82e0942
karenav <karenavr@gmail.com>
2020-09-07 06:28:36 +0000

    using streams instead of for loop

--- portfolio/src/main/java/com/google/sps/servlets/ChartDataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/ChartDataServlet.java
@@ -19,14 +19,17 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
-import java.util.Map;
 import com.google.gson.Gson;
 import com.google.sps.data.Comment;
 import com.google.sps.data.CommentLoader;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.function.Function;
 
 /** Servlet that returns data to use in a chart: listing the words that appeared in the comments 
  * on my portfolio, and the number of times they appeared.
@@ -38,20 +41,13 @@ public final class ChartDataServlet extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     CommentLoader cl = new CommentLoader();
     List<Comment> comments = cl.getComments();
-    Map wordsCount = new HashMap<String, Integer>();
 
-    for (Comment comment : comments) {
-      String[] words = comment.getContent().split(" |!|\\.|\\,|\\?");
-      for (String word : words) {
-        word = word.trim().toLowerCase();
-        if (word.isEmpty()) {
-          continue;
-        }
-        int wordCount = wordsCount.containsKey(word) ? (int) wordsCount.get(word) : 0;
-        wordCount += 1;
-        wordsCount.put(word, wordCount);
-      }
-    }
+    Map<String, Long> wordsCount =
+      comments.stream().map(c -> c.getContent()).map(  // Take content of each comment)
+        c -> Arrays.asList(c.split(" |!|\\.|\\,|\\?")) // Split each comment to words
+      ).flatMap(List::stream).map(c -> c.trim()).filter(p -> !p.isEmpty()).collect( // Flatten, trim, and filter empty words
+        Collectors.groupingBy(p -> p.toLowerCase(), Collectors.counting()) // Count words appearances   
+      );    
 
     response.setContentType("application/json");
     response.getWriter().write(new Gson().toJson(wordsCount));
